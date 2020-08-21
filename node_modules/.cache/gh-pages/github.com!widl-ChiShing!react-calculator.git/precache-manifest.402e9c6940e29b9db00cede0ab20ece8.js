0c55ee032600ae55457d9a7149449d8fe750790d
Chi Shing Poon <cxp@CSP.attlocal.net>
2020-08-21 02:59:20 -0500

    Added server.js

---
+++ node_modules/.cache/gh-pages/github.com!widl-ChiShing!react-calculator.git/precache-manifest.402e9c6940e29b9db00cede0ab20ece8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b819141f6dd0a924913f19a96b2d0499",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/react-calculator/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "30e5093056884849a99f",
+    "url": "/react-calculator/static/js/main.4a674da0.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
