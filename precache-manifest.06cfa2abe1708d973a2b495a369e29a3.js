edbf4040ddfac8bdcc8a4bc1c72dd98b5fbf8f0a
Chi Shing Poon <cxp@CSP.attlocal.net>
2020-08-21 12:39:10 -0500

    Updates

--- precache-manifest.06cfa2abe1708d973a2b495a369e29a3.js
+++ precache-manifest.06cfa2abe1708d973a2b495a369e29a3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8226b19d6dd15d7532ea9d17dfe3a3ec",
+    "revision": "24751ade0686440779042a81cae44755",
     "url": "/react-calculator/index.html"
   },
   {
