b4903ca9f5c9319db7bbaa5c55ff36e0beb36293
Chi Shing Poon <cxp@CSP.attlocal.net>
2020-08-23 01:22:24 -0500

    Add Function to view history, changed to react-bootstrap

---
+++ node_modules/.cache/gh-pages/github.com!widl-ChiShing!react-calculator.git/precache-manifest.3cc3577a281c7f36b92567434714e7ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760e25eac3e4e8089f8b745c1be20f44",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "cfcd6756c06df2a6f683",
+    "url": "/react-calculator/static/js/2.b42ed27a.chunk.js"
+  },
+  {
+    "revision": "2816d2fbf789e8c3d8cd",
+    "url": "/react-calculator/static/js/main.f6f26f22.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
