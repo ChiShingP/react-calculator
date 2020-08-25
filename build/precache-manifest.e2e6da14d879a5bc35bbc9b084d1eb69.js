b72468a32e469f1e689c85cd0b0672d19562b4a7
Chi Shing Poon <cxp@CSP.attlocal.net>
2020-08-25 03:01:45 -0500

    .

---
+++ build/precache-manifest.e2e6da14d879a5bc35bbc9b084d1eb69.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749f872242a645114698f017250f1633",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "d98cd67418ec647bc4e7",
+    "url": "/react-calculator/static/js/2.c7f9e07f.chunk.js"
+  },
+  {
+    "revision": "aea00c70253385c84973",
+    "url": "/react-calculator/static/js/main.8b3523d3.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
